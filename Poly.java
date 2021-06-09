2eefbf4953716e50c13a5bd939cead4a599ac2ae
Yashveer Sekhon <73847249+Yashveer1212@users.noreply.github.com>
2021-06-09 19:27:36 +0000

    made a poly class

---
+++ Poly.java
@@ -0,0 +1,17 @@
+class DiffPiece {
+  public void DiffPieceSound() {
+    System.out.println("Thier are two differnt color of pieces in this checkers project.");
+  }
+}
+
+class White extends DiffPiece {
+  public void DiffPieceSound() {
+    System.out.println("White Piece");
+  }
+}
+
+class Red extends DiffPiece {
+  public void DiffPieceSound() {
+    System.out.println("Red Piece");
+  }
+}
